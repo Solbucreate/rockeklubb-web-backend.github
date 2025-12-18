28a1149c7c15afca292ec9686d8de408239f2647
Solbucreate <mortensolbu@gmail.com>
2025-12-18 13:51:42 +0100

    Fix package.json for Render

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,22 +1,23 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-if (!process.env.DATABASE_URL) {
-  console.error("❌ ERROR: DATABASE_URL mangler i .env!");
-  process.exit(1);
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error("❌ DATABASE_URL is not set");
 }
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
-  protocol: "postgres",
   logging: false,
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
-  },
+  }
 });
 
 export default sequelize;
